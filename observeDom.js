c93c2872c07414ced20ada655e1678e583d0a48a
福田一永 <kazuhisa.fukuda@sprocket.bz>
2019-09-11 22:44:28 +0900

    add functiom

--- observeDom.js
+++ observeDom.js
@@ -1,24 +1,73 @@
 (function(){
-    console.log('start');
+  'use strict';
 
-    // 対象とするノードを取得
-    const target = document.querySelector('body');
+  class ObserveDom {
+    constructor() {
+    }
 
-    // オブザーバインスタンスを作成
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            console.log(mutation);
-        });
-    });
+    observeDom(selector) {
+      let target = document.querySelector(selector);
 
-    // オブザーバの設定
-    const config = {
-        characterData: true,
-        subtree: true,
-        characterDataOldValue: true,
-        characterData: true
-    };
+      console.log('start');
 
-    // 対象ノードとオブザーバの設定を渡す
-    observer.observe(target, config);
+      // オブザーバインスタンスを作成
+      const observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+              console.log(mutation);
+          });
+      });
+
+      // オブザーバの設定
+      const config = {
+          characterData: true,
+          subtree: true,
+          characterDataOldValue: true,
+          characterData: true
+      };
+
+      // 対象ノードとオブザーバの設定を渡す
+      observer.observe(target, config);
+
+      //window.open('popup.html');
+    }
+  }
+
+  class GetRule {
+    constructor() {
+    }
+    /*
+    placeholder() {
+      let a = `curl -X POST
+        -c cookie.txt
+        --data-urlencode 'email=analytics-common@sprocket.bz'
+        --data-urlencode 'password=Ms(rJuX1_Mg?986^'
+        http://api.v2.sprocket.bz/auth/login`;
+      $.get(a)
+      .done(function( data ) {
+        log(data);
+      });
+    }
+    test(){
+      var request = require('request');
+
+      var options = {
+          url: 'http://api.v2.sprocket.bz/auth/login',
+          method: 'POST',
+          password: 'password=Ms(rJuX1_Mg?986^'
+      };
+
+      function callback(error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body);
+          }
+          console.log(response);
+      }
+
+      request(options, callback);
+    }
+    */
+  }
+  const main = new ObserveDom();
+  main.observeDom('body');
+
 })();
